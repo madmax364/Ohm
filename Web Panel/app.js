8ea3284e732bca297c5970ac64b3f95477fc415c
madmax364 <83054089+madmax364@users.noreply.github.com>
2025-11-13 10:28:36 -0500

    Update app.js

--- Web Panel/app.js
+++ Web Panel/app.js
@@ -1,15 +1,14 @@
 ///Edit Under This only
 
-    var firebaseConfig = {
-    apiKey: "API KEY",
-    authDomain: "AUTH DOMAIN",
-    databaseURL: "DATABSE URL",
-    projectId: "PROJECT ID",
-    storageBucket: "BUCKET URL",
-    messagingSenderId: "SENDER ID",
-    appId: "APP ID"
-  }; 
- 
+const firebaseConfig = {
+  apiKey: "AIzaSyCLbUaVnWpvwpQ9cxPNcm9GCr4rtAxISgE",
+  authDomain: "ohm-main.firebaseapp.com",
+  databaseURL: "https://ohm-main-default-rtdb.firebaseio.com",
+  projectId: "ohm-main",
+  storageBucket: "ohm-main.firebasestorage.app",
+  messagingSenderId: "920255544810",
+  appId: "1:920255544810:web:72026124c7d35b87408693"
+};
 ////Do Not Edit Below This
 
 firebase.initializeApp(firebaseConfig);
